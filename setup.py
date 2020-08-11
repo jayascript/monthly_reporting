dbb1fed27061601d734f48452ef10996360ae675
Jayascript <39142547+jayascript@users.noreply.github.com>
2020-08-11 13:46:33 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Automate data collection & extraction and report generation for monthly social media analysis.',
+    author='jayascript',
+    license='MIT',
+)
